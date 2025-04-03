6ce6bc99165dbdb103ace30207240c7185ee9848
Karan Charakatle <karan.charakatle@thinkitive.com>
2025-04-03 11:56:41 +0530

    worked on variable and Scope

---
+++ scopeOfVariables.js
@@ -0,0 +1,15 @@
+// can be reassigned let and var //
+var name='subodh';
+name='karan';
+console.log(name);
+
+let age=21;
+age=22;
+console.log(age);
+
+// can be reassigned let and var //
+var demo1='ogval';
+var demo1 ='newval';
+console.log(demo1);
+
+
